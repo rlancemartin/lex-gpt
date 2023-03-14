ac57fa7318e5b35e6ef25e55594eae3fe37152d3
rlm <31treehaus@31s-MacBook-Pro.local>
2023-03-13 18:05:09 -0700

    set OpenAI key

--- pages/api/vectordbqa.ts
+++ pages/api/vectordbqa.ts
@@ -7,10 +7,11 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {};
 const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  try {
+  // try {
     
       const prompt = req.body.prompt;
       const apiKey = req.body.apiKey;
+      process.env.OPENAI_API_KEY = apiKey;
 
       // Vector DB call
       const pinecone = new PineconeClient();
@@ -18,20 +19,20 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
         environment: "us-east1-gcp", 
         apiKey: process.env.PINECONE_API_KEY ?? "",
       });
-      const index = pinecone.Index("lex-gpt");
+      const index = pinecone.Index("lex-test");
       const vectorStore = await PineconeStore.fromExistingIndex(
         index,
         new OpenAIEmbeddings()
       );
 
       // Call LLM 
-      const model = new OpenAIChat({ openAIApiKey: apiKey, temperature: 0.0 });
+      const model = new OpenAIChat({ temperature: 0.0 });
       const chain = VectorDBQAChain.fromLLM(model, vectorStore);
       chain.returnSourceDocuments=true;
       chain.k=7;
       const responseBody = await chain.call({query: prompt,});
 
-      // Clean up response text
+      // Clean up response
       let answerText = responseBody["text"]; 
       answerText = answerText.replace(/["'\n\r]/g, ''); 
       
@@ -41,10 +42,10 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
       Object.keys(headers).forEach(key => {res.setHeader(key, headers[key]);}); 
       res.status(200).send(answerText); 
       
-  } catch (error) {
-    console.error(error);
-    res.status(500).json("Error");
-  }
+  //} catch (error) {
+  //  console.error(error);
+  //  res.status(500).json("Error");
+  //}
 };
 
 export default handler;
